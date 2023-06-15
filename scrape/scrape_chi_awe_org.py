674615f96be06f96203a06b74e90c254671f082a
Shivam Mishra <shivam.mishra605@gmail.com>
2023-06-15 11:23:07 -0500

    add method to scrape https://www.chi-awe.org/ website

---
+++ scrape/scrape_chi_awe_org.py
@@ -0,0 +1,52 @@
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+class ScrapeChiAWE:
+  def __init__(self) -> None:
+     self.base_url = 'https://www.chi-awe.org/'
+
+  def get_embedded_hyperlinks(self):
+    # make request to the base website
+    resposen = requests.get(self.base_url)
+
+    # Create a BeautifulSoup object to parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find all anchor tags containing hyperlinks
+    anchors = soup.find_all('a')
+
+    # Extract the hyperlink and associated text
+    embedded_hyperlinks_list = []
+    for anchor in anchors:
+        link = anchor.get('href')
+        text = anchor.get_text()
+        if link:
+            embedded_hyperlinks_list.append({'text': text, 'link': link})
+
+    return embedded_hyperlinks_list
+
+  def get_chi_awe_hyperlinks(self, embedded_hyperlinks_list):
+    chi_awe_hyperlinks_dict = {}
+    for d in embedded_hyperlinks_list:
+      text = d['text'].strip()
+      hyperlink = d['link']
+      if hyperlink[0] == '/':
+        chi_awe_hyperlinks_dict[text] = self.base_url + hyperlink[1:]
+    return chi_awe_hyperlinks_dict
+
+  def get_content(self, scrape_class = "sqs-html-content"):
+    embedded_hyperlinks_list = self.get_embedded_hyperlinks()
+    chi_awe_hyperlinks_dict = self.get_chi_awe_hyperlinks(embedded_hyperlinks_list)
+
+    scrape_class_dict = {}
+    scrape_text_dict = {}
+
+    for k, url in chi_awe_hyperlinks_dict.items():
+      response = requests.get(url)
+      soup = BeautifulSoup(response.content, 'html.parser')
+      scrape_class_dict[(k, url)] = soup.find_all("div", {"class": scrape_class})
+      scrape_text_dict[(k, url)] = [x.get_text() for x in scrape_class_dict[(k, url)]]
+
+    return scrape_text_dict
