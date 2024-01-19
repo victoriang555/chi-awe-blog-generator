3e95c9c02e1d8466fd0244df3e21d3b1be4617af
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-01-19 01:13:53 -0600

    Update get_image.py

--- get_image.py
+++ get_image.py
@@ -1,4 +1,6 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=key)
 
 ###### Below are some of the iterations of the prompt before we figured out the best way to phrase the prompt #######
 
@@ -18,12 +20,9 @@ def get_image(key, adj_of_image_type, type_of_image, adj_of_objects, objects_in_
     output - Image URL 
     '''
     PROMPT = "A " + adj_of_image_type + type_of_image + "of" + adj_of_objects +  objects_in_image + verb_of_objects
-    openai.api_key = key
 
-    response = openai.Image.create(
-        prompt=PROMPT,
-        n=1,
-        size="256x256", #can be changed to 512x512, 1024x1024
-    )
-    print(response["data"][0]["url"]);
-    return response["data"][0]["url"]
+    response = client.images.generate(prompt=PROMPT,
+    n=1,
+    size="256x256")
+    print(response.data[0].url);
+    return response.data[0].url
